246e92bc82a41b7164d2fd706e2f09362f92ab8d
aj-codes <ajbarker@wpi.edu>
2021-03-22 10:43:39 -0400

    First Save. Making good progress

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-
+const fighto = document.querySelector("#fight");
+const healthy = document.querySelector("#hp");
+const healtho = document.querySelector("#hp2");
 const allAil = document.getElementsByClassName("U");
 const arrAliens = [
     document.getElementById("enemy_1"),
@@ -8,15 +10,85 @@ const arrAliens = [
     document.getElementById("enemy_5"),
     document.getElementById("enemy_6"),
 ];
-arrAliens[0,5].style.display('none');
+for (i=1;i<arrAliens.length;i++){
+    arrAliens[i].classList.add("hidden");
+}
 
 class Me{
-    
-}
-class Aliens {
     constructor(name,HP,attacks){
         this.name=name;
         this.HP=HP;
         this.attacks=attacks;
     }
+    announceHealth(){
+        healthy.innerText=(this.HP);
+    }
+    fight(enemyName){
+        const currentAtk= this.attacks[Math.round(Math.random()*this.attacks.length)];
+        if(Math.random()<this.attacks.accuracy){
+        console.log(this.name,"used",currentAtk.name,'!');
+            enemyName.HP-=(currentAtk.damage);
+            enemyName.announceHealth();
+        }
+        else{
+
+        }
+    }
 }
+    
+class Aliens {
+    constructor(name,HP,attacks,accuracy){
+        this.name=name;
+        this.HP=HP;
+        this.attacks=attacks;
+        this.accuracy=accuracy;
+    }
+    announceHealth(){
+        healtho.innerText=(this.HP);
+    }
+    fight(DummyMe){
+        const currentAtk= this.attacks[Math.round(Math.random()*this.attacks.length)];
+        console.log(this.name,"used",currentAtk.name,'!');
+            DummyMe.HP-=(currentAtk.damage);
+            DummyMe.announceHealth();      
+    }
+}
+
+//Make My Ship
+const meee = new Me("S.S. Bumpkins",100,[
+    {name:"Blaster",
+    damage:15,
+    accuracy:.95
+},
+    {name:"Big Missile",
+    damage:25,
+    accuracy:.5
+}
+]);
+
+//Make my Enemies
+arrAliens[0] = new Aliens("Dingly",25,[{
+    name:"Shock",
+    damage:5
+},{
+    name:"Psycho Knife",
+    damage:10
+}
+]);
+
+
+let corr=0;
+
+if (corr==5 && arrAliens[corr].HP<=0){
+    healthy.innerText=("You've Won!");
+}
+else if(arrAliens[corr].HP<=0){
+    arrAliens[corr].classList.add("hidden");
+    corr+=1;
+    arrAliens[corr].classList.remove("hidden");
+}
+
+
+
+
+fighto.addEventListener('click',announceHealth())
